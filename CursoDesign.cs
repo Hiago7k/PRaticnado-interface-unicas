170ecb2c6774402d862bc6cd41490c70de62be28
Hiago 7k <hiago7k@gmail.com>
2026-03-05 22:32:16 -0300

    criando curso de sign

---
+++ CursoDesign.cs
@@ -0,0 +1,24 @@
+﻿namespace PRaticnado_interface_unica;
+
+internal class CursoDesign : ICurso
+{
+    public Instrutor instrutor;
+    public string NomeDoCurso { get; set; }
+
+    public CursoDesign(string nomedocruso, Instrutor instrutor)
+    {
+        NomeDoCurso = nomedocruso;
+        this.instrutor = instrutor;
+
+    }
+
+    public void ValidarConteudo()
+    {
+        Console.WriteLine($"Validando conteudo {NomeDoCurso}");
+    }
+
+    public void PublicarCurso()
+    {
+        Console.WriteLine($"Curso publicado com sucesso: DE DESIGN {NomeDoCurso} {instrutor.Especialidade}");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,4 @@
-﻿Console.WriteLine("Iniciando novo projeto");
+﻿using PRaticnado_interface_unica;
+
+Instrutor instrutor1 = new Instrutor("Carla", "Back-End");
+ICurso curso1 = new CursoProgramacao("C# com POO", instrutor1);
