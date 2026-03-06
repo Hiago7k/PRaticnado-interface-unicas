0570f6cc772867eede3191278f4612747dbdc985
Hiago 7k <hiago7k@gmail.com>
2026-03-05 22:17:58 -0300

    criando classe Insturtor

---
+++ Instrutor.cs
@@ -0,0 +1,15 @@
+﻿namespace PRaticnado_interface_unica;
+
+internal class Instrutor
+{
+    public Instrutor(string nome, string instrutor)
+    {
+        Nome = nome;
+        Instrutor = instrutor;
+    }
+
+    public string Nome { get; }
+    public string Instrutor { get; }
+
+
+}
